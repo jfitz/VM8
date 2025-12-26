dc73b701dafcfa13de15578d0165444f26a153df
John Fitzpatrick <jfitz@computer.org>
2025-12-25 18:56:25 -0800

    Add CP/M kernel source.

---
+++ cpm_kernel.asm
@@ -0,0 +1,51 @@
+# CPM kernel
+
+.equate CPM_START   0xf300
+
+.address CPM_START
+
+# call handler for function in C
+DISPATCH
+    LXI H,TABLE
+    CPI C,TABLE_CT
+    JP  CMD_ERR
+    MOV B,0
+    DAD B
+    PCHL
+    
+CMD_ERR
+    RET
+
+# program exit
+C_EXIT
+	RET
+
+# console input char
+CON_IN
+    RET
+
+# console output char
+CON_OUT
+    OUT 1
+    RET
+
+# console output line
+CON_OUT
+    OUT 9
+    RET
+
+TABLE
+.dw C_EXIT      # 0x00
+.dw CON_IN      # 0x01
+.dw CON_OUT     # 0x02
+.dw 0           # 0x03
+.dw 0           # 0x04
+.dw 0           # 0x05
+.dw 0           # 0x06
+.dw 0           # 0x07
+.dw 0           # 0x08
+.dw CON_OUTS    # 0x09
+
+.equate TABLE_SZ    .-TABLE
+.equate TABLE_CT    TABLE_SZ/2
+
--- cpm_page0.asm
+++ cpm_page0.asm
@@ -1,3 +1,7 @@
+# code for CP/M page 0
+
+.include cpm_defs.a
+
 .address 0x0000
 
 	HLT
